22f15b03da8c317d5ae42d4d46f1f8bdf9dc4c9c
hyebin-yoo <hyebin218@naver.com>
2022-06-08 20:02:18 +0900

    Create student_info.h

---
+++ 5.파일의_데이터를_읽어_이중_연결_리스트_구성/student_info.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+typedef struct Node {
+	unsigned char student_id[20];
+	unsigned char department_id;
+	unsigned char department_name[40];
+	unsigned char grade;
+	unsigned char name[80];
+	unsigned char birthday[20];
+
+	struct Node* next;
+	struct Node* prev;
+} Node;
